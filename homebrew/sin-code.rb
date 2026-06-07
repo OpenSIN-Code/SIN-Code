5fc608193cac8795d7f37a9593e15095c8cc2b6c
Delqhi <delqhi@users.noreply.github.com>
2026-06-07 11:53:45 +0200

    fix(homebrew): update SHA-256 checksums for v1.0.2-sin-code release

--- homebrew/sin-code.rb
+++ homebrew/sin-code.rb
@@ -6,20 +6,20 @@ class SinCode < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/OpenSIN-Code/SIN-Code-Bundle/releases/download/v1.0.2-sin-code/sin-code-darwin-arm64.tar.gz"
-      sha256 "f8f7a8062b24be580a567f1dfd64cd8177649b9a7a10c62002aa03daddcbce87"
+      sha256 "a562dace23f609f3a7e615c2dacf84ca62eb7538f5e976131b5a4977b167c44d"
     else
       url "https://github.com/OpenSIN-Code/SIN-Code-Bundle/releases/download/v1.0.2-sin-code/sin-code-darwin-amd64.tar.gz"
-      sha256 "7a31752a399617e515835b6ee683b444d8de16f2c3710e17bb9c651fb9a87c5e"
+      sha256 "4709870c44604bf0cdb42793395b653d08a0335334665dc19ef8f3e7b6b1537f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/OpenSIN-Code/SIN-Code-Bundle/releases/download/v1.0.2-sin-code/sin-code-linux-arm64.tar.gz"
-      sha256 "27f2dc3f6f4d2fadc49451eae81fb8933ce1c2b4061122b7a684fc971ec13f27"
+      sha256 "708519f3c7d8e47468d783205950ac19c7f8b118a974e02cfafcdf938244f4d1"
     else
       url "https://github.com/OpenSIN-Code/SIN-Code-Bundle/releases/download/v1.0.2-sin-code/sin-code-linux-amd64.tar.gz"
-      sha256 "0e69e824fb7a59cd8a62b952fd1928df7f15d2ceae9214869fed3ec132a60439"
+      sha256 "d3df6b46b4436dab6b7481ee1942fc4fee147b608d27c01367fbfcb254d11f05"
     end
   end
 
